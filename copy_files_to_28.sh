08c0cc3b77b24603971e67bbb0413045ecfc5d44
ZhenlongMa <mazhenlong321@gmail.com>
2025-04-19 17:15:50 +0800

    comment copy file

--- copy_files_to_28.sh
+++ copy_files_to_28.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-scp -r collie_based perftest_based python_based simple_script root@10.18.133.28:/work/mazhenlong/rnic_test/
+# scp -r collie_based perftest_based python_based simple_script root@10.18.133.28:/work/mazhenlong/rnic_test/
